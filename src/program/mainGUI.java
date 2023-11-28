cb274cdbb9ff2abe26b7cfbe50f09b2a390018a4
Szabó Roland (SZF_2023_2025) <szab.roland1@diak.szamalk-szalezi.hu>
2023-11-28 11:10:42 +0100

    change mainGUI filename to MainGUI

--- src/program/mainGUI.java
+++ src/program/mainGUI.java
@@ -8,12 +8,12 @@ package program;
  *
  * @author SzabóRoland(SZOFT_20
  */
-public class mainGUI extends javax.swing.JFrame {
+public class MainGUI extends javax.swing.JFrame {
 
     /**
      * Creates new form mainGUI
      */
-    public mainGUI() {
+    public MainGUI() {
         initComponents();
     }
 
@@ -78,20 +78,21 @@ public class mainGUI extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(mainGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MainGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(mainGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MainGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(mainGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MainGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(mainGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MainGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new mainGUI().setVisible(true);
+                new MainGUI().setVisible(true);
             }
         });
     }
